564fe059244b7a993891c1d06cb676879395dde3
fcstoffel <stoffel@undderganzerest.de>
2013-07-09 12:36:47 +0200

    Version 0.6.4
    - fixed Author Link Bug
    
    Signed-off-by: fcstoffel <stoffel@undderganzerest.de>

--- C3S-Meinung.php
+++ C3S-Meinung.php
@@ -5,7 +5,7 @@ $cat = get_theme_mod( 'fcs_gravatar-category' );
 if ($cat && in_category($cat)) : ?>
 
 	<div class="author_avatar">
-		<a href="<?php echo get_the_author_link(); ?>" title="<?php _e('Posts, which are marked as My Opinion reflect the views of the authors and do not necessarily reflect the official view of C3S','origin-c3s') ?>">
+		<a href="<?php echo get_the_author_meta('user_url'); ?>" title="<?php _e('Posts, which are marked as My Opinion reflect the views of the authors and do not necessarily reflect the official view of C3S','origin-c3s') ?>">
 			<?php $user_email = get_the_author_meta('user_email');
 			echo get_avatar( $user_email,60); ?>
 			<span class="byline"><?php _e('My Opinion','origin-c3s') ?></span>
